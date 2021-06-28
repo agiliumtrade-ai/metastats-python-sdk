dc5d0c86c800b6b3b726a41dee0733b12c01cc67
Kostiantyn B <kostiantyn.b.as@agiliumlabs.com>
2021-06-28 10:12:54 +0000

    #PS-386 - fix example and docs

--- examples/metastats/metastats.py
+++ examples/metastats/metastats.py
@@ -1,4 +1,4 @@
-from lib import MetaStats
+from metaapi_cloud_sdk import MetaStats, MetaApi
 import asyncio
 import os
 
@@ -9,11 +9,24 @@ async def get_account_metrics():
     # your MetaApi account id
     account_id = os.getenv('ACCOUNT_ID') or '<put in your account id here>'
 
+    api = MetaApi(token)
     meta_stats = MetaStats(token)
     # you can configure http client via second parameter,
     # see in-code documentation for full definition of possible configuration options
 
     try:
+        account = await api.metatrader_account_api.get_account(account_id)
+
+        #  wait until account is deployed and connected to broker
+        print('Deploying account')
+        if account.state != 'DEPLOYED':
+            await account.deploy()
+        else:
+            print('Account already deployed')
+        print('Waiting for API server to connect to broker (may take couple of minutes)')
+        if account.connection_status != 'CONNECTED':
+            await account.wait_connected()
+
         metrics = await meta_stats.get_metrics(account_id)
         print(metrics)  # -> {'trades': ..., 'balance': ..., ...}
     except Exception as err:
